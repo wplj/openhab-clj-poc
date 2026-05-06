d94f599a3af0e958d46b9ea0d9731c2926049847
max bashyrov <wplj@users.noreply.github.com>
2026-05-06 09:45:48 +0200

    Cover example addon event flow

--- test/example_addon/system_test.clj
+++ test/example_addon/system_test.clj
@@ -1,7 +1,9 @@
 (ns example-addon.system-test
-  (:require [clojure.test :refer [deftest is testing]]
+  (:require [clojure.core.async :as async]
+            [clojure.test :refer [deftest is testing]]
             [example-addon.api :as api]
             [example-addon.system :as system]
+            [openhab.events :as events]
             [openhab.registry :as registry]
             [openhab.reporting :as reporting]))
 
@@ -16,6 +18,18 @@
 (defn- thing-status [ctx thing-id]
   (get-in (registry/get-thing (:registry ctx) thing-id)
           [:runtime :status :value]))
+
+(defn- item-state-topic [item-name]
+  (str "openhab/items/" item-name "/statechanged"))
+
+(defn- await-event-matching [ch pred]
+  (let [deadline (async/timeout 500)]
+    (loop []
+      (let [[event port] (async/alts!! [ch deadline])]
+        (cond
+          (= port deadline) nil
+          (pred event) event
+          :else (recur))))))
 
 (deftest start-projects-initial-device-state
   (reset-api!)
@@ -41,6 +55,44 @@
       (finally
         (system/stop! ctx)))))
 
+(deftest manual-report-publishes-status-and-item-events
+  (api/reset-state! {})
+  (let [ctx          (system/start! {:interval-ms 60000})
+        status-topic "openhab/things/ap-1/statuschanged"
+        status-ch    (events/subscribe-topic! (:bus ctx) status-topic 8)
+        item-chs     (into {}
+                           (map (fn [item-name]
+                                  [item-name
+                                   (events/subscribe-topic! (:bus ctx)
+                                                            (item-state-topic item-name)
+                                                            8)]))
+                           ["AP_FanSpeed" "AP_Temp" "AP_Mode"])]
+    (try
+      (api/reset-state! {"ap-1" {:fan-speed 3
+                                 :temp 21
+                                 :mode "auto"}})
+      (reporting/report-channels! ctx "ap-1" (api/fetch! "ap-1"))
+      (let [status-event (await-event-matching status-ch #(= :online (get-in % [:status :value])))
+            item-events  (mapv (fn [[item-name state]]
+                                  (await-event-matching (get item-chs item-name)
+                                                        #(and (= item-name (:item-name %))
+                                                              (= state (:state %)))))
+                                {"AP_FanSpeed" 3
+                                 "AP_Temp" 21
+                                 "AP_Mode" "auto"})]
+        (is (= "ThingStatusInfoChangedEvent" (:event/name status-event)))
+        (is (= "openhab/things/ap-1/statuschanged" (:event/topic status-event)))
+        (is (= :offline (get-in status-event [:old-status :value])))
+        (is (= :online (get-in status-event [:status :value])))
+        (is (= #{"AP_FanSpeed" "AP_Temp" "AP_Mode"}
+               (set (map :item-name item-events))))
+        (is (= #{3 21 "auto"} (set (map :state item-events)))))
+      (finally
+        (events/unsubscribe-topic! (:bus ctx) status-topic status-ch)
+        (doseq [[item-name ch] item-chs]
+          (events/unsubscribe-topic! (:bus ctx) (item-state-topic item-name) ch))
+        (system/stop! ctx)))))
+
 (deftest item-command-updates-device-and-converges-on-next-report
   (reset-api!)
   (let [ctx (system/start! {:interval-ms 60000})]
@@ -58,6 +110,24 @@
         (is (= {} (get-in (registry/get-thing (:registry ctx) "ap-1")
                           [:runtime :desired]))))
       (finally
+        (system/stop! ctx)))))
+
+(deftest item-command-publishes-optimistic-item-event
+  (reset-api!)
+  (let [ctx        (system/start! {:interval-ms 60000})
+        item-topic (item-state-topic "AP_FanSpeed")
+        item-ch    (events/subscribe-topic! (:bus ctx) item-topic 8)]
+    (try
+      (is (true? (:ok (system/dispatch-command! ctx "AP_FanSpeed" 5))))
+      (let [event (await-event-matching item-ch #(and (= "AP_FanSpeed" (:item-name %))
+                                                      (= 5 (:state %))))]
+        (is (= "ItemStateChangedEvent" (:event/name event)))
+        (is (= "openhab/items/AP_FanSpeed/statechanged" (:event/topic event)))
+        (is (= "AP_FanSpeed" (:item-name event)))
+        (is (= 3 (:old-state event)))
+        (is (= 5 (:state event))))
+      (finally
+        (events/unsubscribe-topic! (:bus ctx) item-topic item-ch)
         (system/stop! ctx)))))
 
 (deftest read-only-item-command-is-rejected-without-changing-device
